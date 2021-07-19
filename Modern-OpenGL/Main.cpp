95e873886a6882e067a391418bdb4a8217405b3f
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-07-19 09:26:29 +0500

    prev change commit

--- Modern-OpenGL/Main.cpp
+++ Modern-OpenGL/Main.cpp
@@ -151,7 +151,7 @@ int main()
     // load and generate the texture
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true);
-    unsigned char* data = stbi_load("C:\\Users\\SALMAN-ALTAF\\Desktop\\wall.jpg", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load("wall.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
