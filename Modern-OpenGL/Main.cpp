c89b9759c14b7122e0731c39ec976ed5efbb67c9
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-08-02 09:15:37 +0500

    added files

--- Modern-OpenGL/Main.cpp
+++ Modern-OpenGL/Main.cpp
@@ -151,10 +151,10 @@ int main()
     // load and generate the texture
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true);
-    unsigned char* data = stbi_load("wall.jpg", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load("wood-texture.png", &width, &height, &nrChannels, 0);
     if (data)
     {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else
